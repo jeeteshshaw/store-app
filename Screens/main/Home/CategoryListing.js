bacbb7f3ea0cc9f9021a4bb6b6adc73ca91e1fd9
jeetesh kumar shaw <jeeteshshaw@gmail.com>
2022-11-02 00:33:16 +0530

    listing screen"

--- Screens/main/Home/CategoryListing.js
+++ Screens/main/Home/CategoryListing.js
@@ -1,13 +1,22 @@
-import { FlatList, Image, ImageBackground, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, Image, ImageBackground, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useCallback } from 'react'
 import { CategoryList } from '../../../store/static'
 import { normalizeSize, SCREEN_WIDTH } from '../../../utility'
 import { Rtext } from '../../../Components/Rtext'
 import { ThemeLightGreen } from '../../../config/Colors'
 import Styles from '../../../Styles'
 import GS from '../../../Styles/GlobalStyle'
+import { useNavigation } from '@react-navigation/native'
 
 const CategoryListing = () => {
+    const navigation = useNavigation();
+    const onSelectCategory = useCallback(
+      () => {
+        return navigation.navigate("ProductListing")
+      },
+      [],
+    )
+    
     return (
         <ImageBackground style={styles.container} source={require("../../../assets/images/linear_gradient_light_green_bg.png")} resizeMode="cover">
 
@@ -20,7 +29,7 @@ const CategoryListing = () => {
                 listKey={"CategoryList"}
 
                 renderItem={({ item, index }) => (
-                    <RenderItemCard item={item} index={index} />
+                    <RenderItemCard item={item} index={index} onPress={onSelectCategory} />
                 )}
             />
         </ImageBackground >
@@ -29,12 +38,12 @@ const CategoryListing = () => {
 
 export default CategoryListing;
 
-const RenderItemCard = React.memo(({ item, index }) => (
+const RenderItemCard = React.memo(({ item, index,onPress }) => (
 
-    <View style={[styles.cardContainer, GS.center]}>
+    <TouchableOpacity onPress={onPress} style={[styles.cardContainer, GS.center]}>
         <Image source={item.image} style={styles.cardImg} />
         <Rtext fontSize={11} style={styles.cardText} numberOfLines={3}>{item.title}</Rtext>
-    </View>
+    </TouchableOpacity>
 ));
 
 
---
+++ Screens/main/ProductListing/index.js
@@ -0,0 +1,56 @@
+import { FlatList, Image, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Styles from '../../../Styles'
+import GS from '../../../Styles/GlobalStyle'
+import { normalizeSize, SCREEN_HEIGHT, SCREEN_WIDTH } from '../../../utility'
+import { Rtext } from '../../../Components/Rtext'
+import { CategoryList } from '../../../store/static'
+import LeftCatrgoryList from './LeftCatrgoryList'
+import { ThemeDarkGreen } from '../../../config/Colors'
+
+const rightHalf = SCREEN_WIDTH - normalizeSize(100)
+
+const ProductListing = () => {
+  return (
+    <View style={GS.container}>
+      <View style={GS.row}>
+        <View style={{ height: SCREEN_HEIGHT, width: normalizeSize(75), alignItems: "center", elevation: 2, backgroundColor: "#fff" }}>
+          <LeftCatrgoryList />
+        </View>
+        <View style={{ flex: 1 }}>
+          <View style={{ flex: 1, flexDirection: "row", marginHorizontal:4 }}>
+
+            <FlatList
+              data={[1,2,3,4,5,6,7,8,9,10]}
+              numColumns={3}
+              renderItem={({index})=>(
+                <View style={{ backgroundColor: "#fff", borderColor:ThemeDarkGreen,borderWidth:1,elevation: 2, width: ((SCREEN_WIDTH - normalizeSize(75)) / 3) - 8, minHeight: normalizeSize(130), margin:4 }}>
+              <Image source={require("../../../assets/images/category1.png")} style={{ height: 80, width: "100%", resizeMode: "stretch", }} />
+              <View style={{ borderWidth: 3, borderColor: "#314602", borderTopColor: "#fff", flex:1, paddingHorizontal: 4 }}>
+                <View style={[GS.container]}>
+                <Rtext fontSize={8}>Lorem ipsum dolor sit amet lorem ipsum dolor sit amet,</Rtext>
+                </View>
+                <View style={[GS.row_Between, { padding: 4 }]}>
+                  <Rtext fontSize={10} style={{ color: "#000" }} numberOfLines={1}>20$</Rtext>
+                  {/* <Rtext fontSize={10} style={{ color:"#fff"}} numberOfLines={1}>20$</Rtext> */}
+                  <View style={[GS.btn, GS.center, { padding:4 }]} >
+                    <Rtext style={{ color: "#fff" }} fontSize={10}>Add</Rtext>
+                  </View>
+
+                </View>
+              </View>
+            </View>
+              )}
+            />
+
+          </View>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+export default ProductListing
+
+const styles = StyleSheet.create({})
+
